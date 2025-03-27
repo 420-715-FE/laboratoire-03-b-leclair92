3c21e181d7a6632b8781225822a1fe788b255059
leclair92 <leclair92@outlook.fr>
2025-03-26 23:07:53 -0400

    Laboratoire 3B - 26-03-2025

--- pages/page_secrete.php
+++ pages/page_secrete.php
@@ -1,3 +1,34 @@
+<?php
+
+session_start();
+
+$utilisateurs = [
+    'jaja72' => ['nom' => 'Jacynthe Laplante', 'motDePasse' => 'lapin'],
+    'petitefleur145' => ['nom' => 'Rose Lafleur', 'motDePasse' => 'chat'],
+    'bob' => ['nom' => "Bob L'éponge", 'motDePasse' => 'poisson'],
+];
+
+
+if (isset($_GET['deconnexion'])) {
+    session_unset();
+    session_destroy();
+}
+
+$erreur = false;
+
+if (isset($_POST['utilisateur']) && isset($_POST['mot_de_passe'])) {
+    $username = $_POST['utilisateur'];
+    $password = $_POST['mot_de_passe'];
+
+    if (isset($utilisateurs[$username]) && $password === $utilisateurs[$username]['motDePasse']) {      
+        $_SESSION['utilisateur'] = $utilisateurs[$username];
+    } else {
+        $erreur = true;
+    }
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -11,5 +42,26 @@
         <a href="../index.php">Retour</a>
     </nav>
     <h1>Page secrète</h1>
+    <?php
+        if (isset($_SESSION['utilisateur'])) {
+        ?>
+            <p>Bonjour <strong><?php echo $_SESSION['utilisateur']['nom'] ;?></strong> ! Bienvenue sur la page secrète!</p>
+            <a href="?deconnexion">Se déconnecter</a></p>
+        <?php
+        } else {
+            if ($erreur) {
+                echo "<p>Nom d'utilisateur ou mot de passe incorrect.</p>";
+            }
+            ?>
+            <form action="page_secrete.php" method="post">
+                <label for="utilisateur">Nom d'utilisateur:</label>
+                <input type="text" name="utilisateur" id="utilisateur">
+                <label for="mot_de_passe">Mot de passe:</label>
+                <input type="password" name="mot_de_passe" id="mot_de_passe">
+                <button type="submit">Envoyer</button>
+            </form>
+            <?php
+        }
+    ?>
 </body>
 </html>
--- pages/tic_tac_toe.php
+++ pages/tic_tac_toe.php
@@ -1,3 +1,77 @@
+<?php
+    session_start();
+
+    if (isset($_GET['reinitialiser'])) {
+        session_unset();
+        session_destroy();
+    }
+
+    if(!isset($_SESSION['tableaux'])){
+        $_SESSION['tableaux'] = ([
+            ['()','()','()'],
+            ['()','()','()'],
+            ['()','()','()'],
+            
+            ]);
+    }
+
+    $tableaux = $_SESSION['tableaux'];
+
+    $joueur = 'x';
+    $message = '';
+
+    if(!isset($_SESSION['joueur'])){
+        $_SESSION['joueur'] = 'x';
+    }
+    $joueur = $_SESSION['joueur'];
+    
+    $ligne = 0;
+    $colonne = 0;
+
+    if(isset($_GET['ligne']) && isset($_GET['colonne']) ){
+        $ligne = htmlspecialchars($_GET['ligne']);
+        $colonne = htmlspecialchars($_GET['colonne']);
+  
+        if($tableaux[$ligne][$colonne] == '()'){
+            $tableaux[$ligne][$colonne] = $joueur;
+            $_SESSION['tableaux'] = $tableaux;
+
+            for($i = 0; $i < 3; $i++){
+
+               //validation verticale
+                if(($tableaux[0][$i] === 'x' && $tableaux[1][$i] === 'x' && $tableaux[2][$i] === 'x') || ($tableaux[0][$i] === 'o' && $tableaux[1][$i] === 'o' && $tableaux[2][$i] === 'o')) {
+                    $message = 'Vous avez gagné !';
+                    
+                }
+
+            //validation horizontale
+                if(($tableaux[$i][0] == 'x' && $tableaux[$i][1] == 'x' && $tableaux[$i][2] == 'x') || ($tableaux[$i][0] == 'o' && $tableaux[$i][1] == 'o' && $tableaux[$i][2] == 'o')){
+                    $message = 'Vous avez gagné !';
+                    
+                }
+
+                
+            //validation diagonale vers Droite
+                if(($tableaux[0][0] == 'x' && $tableaux[1][1] == 'x' && $tableaux[2][2] == 'x') || ($tableaux[0][0] == 'o' && $tableaux[1][1] == 'o' && $tableaux[2][2] == 'o')){
+                    $message = 'Vous avez gagné !';  
+                }
+
+
+            //validation diagonale vers Gauche
+                if(($tableaux[0][2] == 'x' && $tableaux[1][1] == 'x' && $tableaux[2][0] == 'x') || ($tableaux[0][2] == 'o' && $tableaux[1][1] == 'o' && $tableaux[2][0] == 'o')){
+                    $message = 'Vous avez gagné !';
+                    
+                }
+        }
+
+    }
+
+    $_SESSION['joueur'] = ($_SESSION['joueur'] == 'x') ? 'o' : 'x';
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -35,8 +109,20 @@
         <a href="../index.php">Retour</a>
     </nav>
     <h1>Tic Tac Toe</h1>
-    <table class="ticTacToe">
 
+    <?php echo '<p>C\'est le tour des '.$joueur.'</p>';?>
+    <table class="ticTacToe">
+    <?php
+        echo $message;
+        foreach($tableaux as $l => $tableau){
+            echo'<tr>';
+                foreach($tableau as $c => $colonne){
+                    echo'<td><a href="?joueur='.$joueur.'&ligne='.$l.'&colonne='.$c.'">'.$colonne.'</a></td>';
+                }
+            echo'</tr>';
+            }
+    ?>
+    
     </table>
     <a href="?reinitialiser">Réinitialiser</a>
 </body>
